c4a745e0138f442f9c047091e4592e3096d7793b
Eamonn Maguire <eamonnmag@gmail.com>
2015-09-04 00:26:11 +0200

    va-interface: recreated plots with dc.js.
    Will tidy this up and add more tomorrow.

--- thor/modules/dashboard/static/assets/js/thor-dash-lib.js
+++ thor/modules/dashboard/static/assets/js/thor-dash-lib.js
@@ -2,344 +2,158 @@
  * Created by eamonnmaguire on 26/08/15.
  */
 // (It's CSV, but GitHub Pages only gzip's JSON at the moment.)
-d3.json("/api/get_data", function(error, data) {
+d3.json("/api/get_data", function (error, data) {
 
-  // Various formatters.
-  var formatNumber = d3.format(",d"),
-      formatDate = d3.time.format("%B %Y"),
-      formatTime = d3.time.format("%I:%M %p");
+    // Various formatters.
+    var formatNumber = d3.format(",d"),
+        formatDate = d3.time.format("%b %Y"),
+        formatTime = d3.time.format("%I:%M %p");
 
-  // A nest operator, for grouping the flight list.
-  var nestByDate = d3.nest()
-      .key(function(d) { return d3.time.day(d.date); });
+    // A nest operator, for grouping the flight list.
+    var nestByDate = d3.nest()
+        .key(function (d) {
+            return d3.time.day(d.date);
+        });
 
-  // A little coercion, since the CSV is untyped.
-  data.data.forEach(function(d, i) {
-    d.index = i;
-    d.date = parseDate(d.date);
-  });
+    // A little coercion, since the CSV is untyped.
+    data.data.forEach(function (d, i) {
+        d.index = i;
+        d.date = parseDate(d.date);
+        d.total_doi = (d.data_doi_count + d.publication_doi_count);
+        d.total_orcid = (d.data_orcid_count + d.publication_orcid_count);
+    });
+
+    function parseDate(d) {
+        return new Date(d.substring(0, 4),
+            d.substring(5, 7) - 1,
+            d.substring(8));
+    }
 
     console.log(data.data);
 
-  // Create the crossfilter for the relevant dimensions and groups.
-  var records = crossfilter(data.data),
-      all = records.groupAll(),
-      date = records.dimension(function(d) { return d.date; }),
-      dois_date = date.group().reduceSum(function(d) { return d.total_doi;}),
-      months = date.group(d3.time.month),
-      doi = records.dimension(function(d) { return d.total_doi; }),
-      dois = doi.group(Math.floor),
-      orcid = records.dimension(function(d) { return d.total_orcid; }),
-      orcids = orcid.group(Math.floor);
+    // Create the crossfilter for the relevant dimensions and groups.
+    var records = crossfilter(data.data),
+
+        date = records.dimension(function (d) {
+            return d.date;
+        }),
+        dois_date = date.group().reduceSum(function (d) {
+            return d.total_doi;
+        }),
+
+        centre = records.dimension(function(d) {
+           return d.data_centre;
+        }),
+
+        centre_hits = centre.group().reduceSum(function(d) {
+           return d.total_doi;
+        }),
+
+        doi = records.dimension(function (d) {
+            return d.total_doi;
+        }),
+        dois = doi.group(Math.floor),
+        orcid = records.dimension(function (d) {
+            return d.total_orcid;
+        }),
+        orcids = orcid.group(Math.floor);
 
 
-  var charts = [
+    var minDoi = doi.bottom(1)[0].total_doi;
+    var maxDOI = doi.top(1)[0].total_doi;
 
-      dc.barChart()
+    var doiChart = dc.barChart('#doi-chart');
+    doiChart.width(300)
+        .height(150)
+        .margins({top: 10, right: 20, bottom: 30, left: 30})
         .dimension(doi)
         .group(dois)
-      .x(d3.scale.linear()
-        .domain(d3.extent(data.data, function(d) {
-                return d.total_doi;
-            }))
-        .rangeRound([0, 300])),
+        .elasticY(true)
+        .x(d3.scale.linear().domain([Math.min(minDoi,0), maxDOI+10]));
 
-       barChart()
+    doiChart.yAxis().ticks(5);
+
+
+    var minORCID = orcid.bottom(1)[0].total_orcid;
+    var maxORCID = orcid.top(1)[0].total_orcid;
+
+    var orcidChart = dc.barChart('#orcid-chart');
+    orcidChart.width(300)
+        .height(150)
+        .margins({top: 10, right: 20, bottom: 30, left: 30})
         .dimension(orcid)
         .group(orcids)
-      .x(d3.scale.linear()
-        .domain(d3.extent(data.data, function(d) {
-                return d.total_orcid;
-            }))
-        .range([0, 300])),
+        .elasticY(true)
+        .x(d3.scale.linear().domain([Math.min(minORCID,0), maxORCID+10]));
 
 
-       barChart()
+    var minDate = new Date(date.bottom(1)[0].date);
+    var maxDate = new Date(date.top(1)[0].date);
+    minDate.setDate(minDate.getDate()-15);
+    maxDate.setDate(maxDate.getDate()+15);
+
+    var doiDateChart = dc.barChart('#monthly-doi-chart');
+    doiDateChart.width(400)
+        .height(150)
+        .margins({top: 10, right: 20, bottom: 30, left: 30})
         .dimension(date)
         .group(dois_date)
-      .x(d3.scale.linear()
-        .domain(d3.extent(data.data, function(d) {
-                return d.date;
-            }))
-        .range([0, 300]))
+        .elasticY(true)
+        .x(d3.scale.linear().domain([minDate, maxDate]));
 
-  ];
-
-  // Given our array of charts, which we assume are in the same order as the
-  // .chart elements in the DOM, bind the charts to the DOM and render them.
-  // We also listen to the chart's brush events to update the display.
-  var chart = d3.selectAll(".chart")
-      .data(charts)
-      .each(function(chart) { chart.on("brush", renderAll).on("brushend", renderAll); });
-
-  // Render the initial lists.
-  var list = d3.selectAll(".list")
-      .data([recordList]);
-
-  // Render the total.
-  d3.selectAll("#total")
-      .text(formatNumber(records.size()));
-
-  renderAll();
-
-  // Renders the specified chart or list.
-  function render(method) {
-    d3.select(this).call(method);
-  }
-
-  // Whenever the brush moves, re-rendering everything.
-  function renderAll() {
-    chart.each(render);
-    list.each(render);
-    d3.select("#active").text(formatNumber(all.value()));
-  }
-
-  // Like d3.time.format, but faster.
-  function parseDate(d) {
-    return new Date(d.substring(0, 4),
-        d.substring(5, 7) - 1,
-        d.substring(8));
-  }
-
-  window.filter = function(filters) {
-    filters.forEach(function(d, i) { charts[i].filter(d); });
-    renderAll();
-  };
-
-  window.reset = function(i) {
-    charts[i].filter(null);
-    renderAll();
-  };
-
-  function recordList(div) {
-    var recordsByDate = nestByDate.entries(date.top(40));
-
-    div.each(function() {
-      var date = d3.select(this).selectAll(".date")
-          .data(recordsByDate, function(d) { return d.key; });
-
-      date.enter().append("div")
-          .attr("class", "date")
-        .append("div")
-          .attr("class", "day")
-          .text(function(d) { return formatDate(d.values[0].date); });
-
-      date.exit().remove();
-
-      var record = date.order().selectAll(".record")
-          .data(function(d) { return d.values; }, function(d) { return d.index; });
-
-      var recordEnter = record.enter().append("div")
-          .attr("class", "record");
+    doiDateChart.xAxis().tickFormat(
+        function (v) {
+            return formatDate(new Date(v));
+        });
+    doiDateChart.xAxis().ticks(6);
 
 
-      recordEnter.append("div")
-          .attr("class", "data_centre")
-          .text(function(d) { return d.data_centre; });
+    var doiCentreChart = dc.pieChart('#doi-centre-chart');
+    doiCentreChart.width(150)
+        .height(150)
+        .dimension(centre)
+        .group(centre_hits);
 
-      recordEnter.append("div")
-          .attr("class", "distance")
-          .text(function(d) { return formatNumber(d.total_doi) + " DOIs"; });
 
-      recordEnter.append("div")
-          .attr("class", "delay")
-          .classed("early", function(d) { return d.total_orcid < 0; })
-          .text(function(d) { return d.total_orcid + " ORCIDs (" + d.data_orcid_count + " data, " + d.publication_orcid_count + " publications)"; });
+    var detailTable = dc.dataTable('.dc-data-table');
 
-      record.exit().remove();
+    detailTable.dimension(date)
+        .group(function (d) {
+            return formatDate(d.date);
+        })
+        .columns([
+            function () {
+                return ""
+            },
+            function (d) {
+                return d.data_centre
+            },
+            function (d) {
+                return d.total_doi
+            },
+            function (d) {
+                return d.data_doi_count
+            },
+            function (d) {
+                return d.publication_doi_count
+            },
+            function (d) {
+                return d.total_orcid
+            },
+            function (d) {
+                return d.data_orcid_count
+            },
+            function (d) {
+                return d.publication_orcid_count
+            }
+        ])
+    ;
 
-      record.order();
-    });
-  }
 
-  function barChart() {
-    if (!barChart.id) barChart.id = 0;
+    // Render the total.
+    //d3.selectAll("#total")
+    //    .text(formatNumber(records.size()));
 
-    var margin = {top: 10, right: 10, bottom: 20, left: 10},
-        x,
-        y = d3.scale.linear().range([100, 0]),
-        id = barChart.id++,
-        axis = d3.svg.axis().orient("bottom"),
-        brush = d3.svg.brush(),
-        brushDirty,
-        dimension,
-        group,
-        round;
+    dc.renderAll();
 
-    function chart(div) {
-      var width = x.range()[1],
-          height = y.range()[0];
-        y.domain([0, group.top(1)[0].value]);
-
-      div.each(function() {
-        var div = d3.select(this),
-            g = div.select("g");
-
-        // Create the skeletal chart.
-        if (g.empty()) {
-          div.select(".title").append("a")
-              .attr("href", "javascript:reset(" + id + ")")
-              .attr("class", "reset")
-              .text("reset")
-              .style("display", "none");
-
-          g = div.append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-          g.append("clipPath")
-              .attr("id", "clip-" + id)
-            .append("rect")
-              .attr("width", width)
-              .attr("height", height);
-
-          g.selectAll(".bar")
-              .data(["background", "foreground"])
-            .enter().append("path")
-              .attr("class", function(d) { return d + " bar"; })
-              .datum(group.all());
-
-          g.selectAll(".foreground.bar")
-              .attr("clip-path", "url(#clip-" + id + ")");
-
-          g.append("g")
-              .attr("class", "axis")
-              .attr("transform", "translate(0," + height + ")")
-              .call(axis);
-
-          // Initialize the brush component with pretty resize handles.
-          var gBrush = g.append("g").attr("class", "brush").call(brush);
-          gBrush.selectAll("rect").attr("height", height);
-          gBrush.selectAll(".resize").append("path").attr("d", resizePath);
-        }
-
-        // Only redraw the brush if set externally.
-        if (brushDirty) {
-          brushDirty = false;
-          g.selectAll(".brush").call(brush);
-          div.select(".title a").style("display", brush.empty() ? "none" : null);
-          if (brush.empty()) {
-            g.selectAll("#clip-" + id + " rect")
-                .attr("x", 0)
-                .attr("width", width);
-          } else {
-            var extent = brush.extent();
-            g.selectAll("#clip-" + id + " rect")
-                .attr("x", x(extent[0]))
-                .attr("width", x(extent[1]) - x(extent[0]));
-          }
-        }
-
-        g.selectAll(".bar").attr("d", barPath);
-      });
-
-      function barPath(groups) {
-        var path = [],
-            i = -1,
-            n = groups.length,
-            d;
-        while (++i < n) {
-          d = groups[i];
-          path.push("M", x(d.key), ",", height, "V", y(d.value), "h9V", height);
-        }
-        return path.join("");
-      }
-
-      function resizePath(d) {
-        var e = +(d == "e"),
-            x = e ? 1 : -1,
-            y = height / 3;
-        return "M" + (.5 * x) + "," + y
-            + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6)
-            + "V" + (2 * y - 6)
-            + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y)
-            + "Z"
-            + "M" + (2.5 * x) + "," + (y + 8)
-            + "V" + (2 * y - 8)
-            + "M" + (4.5 * x) + "," + (y + 8)
-            + "V" + (2 * y - 8);
-      }
-    }
-
-    brush.on("brushstart.chart", function() {
-      var div = d3.select(this.parentNode.parentNode.parentNode);
-      div.select(".title a").style("display", null);
-    });
-
-    brush.on("brush.chart", function() {
-      var g = d3.select(this.parentNode),
-          extent = brush.extent();
-      if (round) g.select(".brush")
-          .call(brush.extent(extent = extent.map(round)))
-        .selectAll(".resize")
-          .style("display", null);
-      g.select("#clip-" + id + " rect")
-          .attr("x", x(extent[0]))
-          .attr("width", x(extent[1]) - x(extent[0]));
-      dimension.filterRange(extent);
-    });
-
-    brush.on("brushend.chart", function() {
-      if (brush.empty()) {
-        var div = d3.select(this.parentNode.parentNode.parentNode);
-        div.select(".title a").style("display", "none");
-        div.select("#clip-" + id + " rect").attr("x", null).attr("width", "100%");
-        dimension.filterAll();
-      }
-    });
-
-    chart.margin = function(_) {
-      if (!arguments.length) return margin;
-      margin = _;
-      return chart;
-    };
-
-    chart.x = function(_) {
-      if (!arguments.length) return x;
-      x = _;
-      axis.scale(x);
-      brush.x(x);
-      return chart;
-    };
-
-    chart.y = function(_) {
-      if (!arguments.length) return y;
-      y = _;
-      return chart;
-    };
-
-    chart.dimension = function(_) {
-      if (!arguments.length) return dimension;
-      dimension = _;
-      return chart;
-    };
-
-    chart.filter = function(_) {
-      if (_) {
-        brush.extent(_);
-        dimension.filterRange(_);
-      } else {
-        brush.clear();
-        dimension.filterAll();
-      }
-      brushDirty = true;
-      return chart;
-    };
-
-    chart.group = function(_) {
-      if (!arguments.length) return group;
-      group = _;
-      return chart;
-    };
-
-    chart.round = function(_) {
-      if (!arguments.length) return round;
-      round = _;
-      return chart;
-    };
-
-    return d3.rebind(chart, brush, "on");
-  }
 });
