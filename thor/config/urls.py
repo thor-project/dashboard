745ca17febb16c39ce5fefc484d38a6404c63ed4
Eamonn Maguire <eamonnmag@gmail.com>
2015-09-16 11:00:38 +0200

    dashboard: fixing links for initial version.

--- thor/config/urls.py
+++ thor/config/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,8 +9,9 @@ urlpatterns = patterns('',
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
         'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-
+    url(r'^$', RedirectView.as_view(url='/dashboard/data/')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^dashboard/', include('thor.modules.dashboard.urls')),
+
     url(r'api/', include('thor.modules.api.urls')),
 )
--- thor/modules/dashboard/urls.py
+++ thor/modules/dashboard/urls.py
@@ -1,9 +1,12 @@
+from django.views.generic import RedirectView
+
 __author__ = 'eamonnmaguire'
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'thor.modules.dashboard.views.dashboard', name='home'),
+
+    url(r'^$', RedirectView.as_view(url='/dashboard/data/')),
     url(r'^data/', 'thor.modules.dashboard.views.data_dashboard', name='data'),
     url(r'^researcher/', 'thor.modules.dashboard.views.researcher_dashboard', name='researcher')
 
