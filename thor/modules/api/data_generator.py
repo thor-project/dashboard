0b4ab26a96220c3b3651cb9d001ec4da9d4544c2
Eamonn Maguire <eamonnmag@gmail.com>
2015-09-16 16:45:12 +0200

    data generator: creating a more weighted generator

--- thor/modules/api/data_generator.py
+++ thor/modules/api/data_generator.py
@@ -9,17 +9,26 @@ Simple script to generate some random data for front end testing purposes.
 """
 
 metric_types = {
-"orcid": [{"name": "Oxford University", "country": "United Kingdom"}, {"name": "CERN", "country": "Switzerland"},
-          {"name": "Imperial College London", "country": "United Kingdom"}, {"name": "Lund University", "country": "Sweden"}, {"name": "KU Leuven", "country": "Belgium"},
-          {"name": "Newcastle University", "country": "United Kingdom"}
-    , {"name": "MIT", "country": "United States"}],
-"doi": [{"name": "BL", "country": "United Kingdom"}, {"name": "CDL.DRYAD", "country": "United States"},
-        {"name": "DATACITE", "country": "United Kingdom"}, {"name": "TIB.PANGEA", "country": "Sweden"},
-        {"name": "ANDS", "country": "Australia"}, {"name": "CERN", "country": "Switzerland"}]
+    "orcid": [{"name": "Oxford University", "country": "United Kingdom", "max": 320, "types": ["publication"]},
+              {"name": "CERN", "country": "Switzerland", "max": 250, "types": ["publication", "data"]},
+              {"name": "Imperial College London", "country": "United Kingdom", "max": 150, "types": ["publication", "data"]},
+              {"name": "Lund University", "country": "Sweden", "max": 100, "types": ["publication", "data"]},
+              {"name": "KU Leuven", "country": "Belgium", "max": 120, "types": ["publication", "data"]},
+              {"name": "Newcastle University", "country": "United Kingdom", "max": 200, "types": ["publication", "data"]},
+              {"name": "MIT", "country": "United States", "max": 200, "types": ["publication", "data"]},
+              {"name": "Harvard University", "country": "United States", "max": 300, "types": ["publication", "data"]}
+              ],
+
+    "doi": [{"name": "BL", "country": "United Kingdom", "max": 1000, "types": ["publication"]},
+            {"name": "CDL.DRYAD", "country": "United States", "max": 1200, "types": ["data"]},
+            {"name": "DATACITE", "country": "United Kingdom", "max": 2000, "types": ["data", "publication"]},
+            {"name": "TIB.PANGEA", "country": "Sweden", "max": 300, "types": ["data", "publication"]},
+            {"name": "ANDS", "country": "Australia", "max": 600, "types": ["publication"]},
+            {"name": "CERN", "country": "Switzerland", "max": 200, "types": ["data", "publication"]}]
 }
 
 years = {"orcid": ["2013", "2014", "2015"], "doi": ["2011", "2012", "2013", "2014", "2015"]}
-data_types = ["publication", "data"]
+
 data_assignments = ["assigned", "unassigned"]
 
 for metric_type in metric_types:
@@ -29,14 +38,17 @@ for metric_type in metric_types:
             for month in range(1, 13):
                 if month < 10:
                     month = "0" + str(month)
-                for data_type in data_types:
+
+                institution_idx = random.randint(0, len(metric_types[metric_type]) - 1)
+                institution = metric_types[metric_type][institution_idx]
+
+                for data_type in institution['types']:
                     data_item = {"date": "{}-{}-{}".format(year, month, "01")}
-                    institution_idx = random.randint(0, len(metric_types[metric_type]) - 1)
-                    data_item["institution"] = metric_types[metric_type][institution_idx]['name']
-                    data_item["country"] = metric_types[metric_type][institution_idx]['country']
+                    data_item["institution"] = institution['name']
+                    data_item["country"] = institution['country']
                     data_item["data_key"] = data_type
                     data_item["assign_status"] = data_assignment
-                    data_item["data_value"] = math.floor(random.randint(1, 1000))
+                    data_item["data_value"] = math.floor(random.randint(1, institution['max']))
 
                     if (metric_type == 'orcid'):
                         data_item["new_orcids"] = math.floor(random.randint(1, 100))
